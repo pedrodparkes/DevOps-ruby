1c56a4051449d1e9ada336bfb5d12b9f0518ade1
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 01:04:32 +0200

    adding some work

---
+++ manifests/site.pp
@@ -0,0 +1,17 @@
+node puppet.ruby {
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
+  # Configure puppet Master to checkout the repo and autosign new nodes
+  class { 'mcpuppet': 
+    repo_dir => "/etc/puppetlabs/code/environments/ruby",
+    ensure   => latest,
+    source   => 'git@github.com:pedrodparkes/DevOps-ruby.git',
+    revision => 'master',
+    autosign => true,
+  }
+}
+
+node bastion.ruby {
+}
