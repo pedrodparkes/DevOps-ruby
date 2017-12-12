e1d7f70eb5ed12369195c9af4db5a3119cb58be2
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 01:36:14 +0200

    adding grafana

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,7 +14,24 @@ node puppet.ruby {
 }
 
 node bastion.ruby {
-  class { 'grafana': }
+  class { 'grafana':
+    cfg => {
+      app_mode => 'production',
+      server   => {
+        http_port     => 8080,
+      },
+      database => {
+        type          => 'sqlite3',
+        host          => '127.0.0.1:3306',
+        name          => 'grafana',
+        user          => 'root',
+        password      => '',
+      },
+      users    => {
+        allow_sign_up => false,
+      },
+    },
+  }
 }
 
 node jenkins.ruby {
