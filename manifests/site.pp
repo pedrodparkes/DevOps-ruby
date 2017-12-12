c8e34547234ccdc60b12d2e3766d31a1040e6c01
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 01:44:58 +0200

    grafana config

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,5 +20,22 @@ node jenkins.ruby {
 }
 
 node grafana.ruby {
-  class { 'grafana': }
+  class { 'grafana':
+    cfg => {
+      app_mode => 'production',
+      server   => {
+        http_port     => 8080,
+      },
+      database => {
+        type          => 'sqlite3',
+        host          => '127.0.0.1:3306',
+        name          => 'grafana',
+        user          => 'root',
+        password      => '',
+      },
+      users    => {
+        allow_sign_up => false,
+      },
+    },
+  }
 }
