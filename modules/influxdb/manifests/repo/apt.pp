8f442385dbc0e4f5597dbb7207467b7b0e96fe75
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 02:27:36 +0200

    adding apt

--- modules/influxdb/manifests/repo/apt.pp
+++ modules/influxdb/manifests/repo/apt.pp
@@ -10,8 +10,8 @@ class influxdb::repo::apt {
     release     => $::lsbdistcodename,
     repos       => 'stable',
     key         => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-    key_source  => 'https://repos.influxdata.com/influxdb.key',
-    include_src => false,
+    # key_source  => 'https://repos.influxdata.com/influxdb.key',
+    # include_src => false,
   }
 
   Apt::Source['repos.influxdata.com'] -> Package<| tag == 'influxdb' |>
