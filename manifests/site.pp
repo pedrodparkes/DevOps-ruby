61efd447aa0848348d6c97beeb9323c1e5402a8a
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 23:05:25 +0200

    adding old version of APT

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,19 +50,7 @@ node grafana.ruby {
   #     package => true,
   #     service => true,
   #   }
-  class { 'apt':
-  always_apt_update    => false,
-  apt_update_frequency => undef,
-  disable_keys         => undef,
-  proxy_host           => false,
-  proxy_port           => '8080',
-  purge_sources_list   => false,
-  purge_sources_list_d => false,
-  purge_preferences_d  => false,
-  update_timeout       => undef,
-  fancy_progress       => undef
-}
-  -> grafana_datasource { 'influxdb':
+  grafana_datasource { 'influxdb':
     grafana_url       => 'http://localhost:8080',
     grafana_user      => 'admin',
     grafana_password  => 'gfhfcjkmrf',
