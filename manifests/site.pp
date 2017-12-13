f743d1166769d4f6b8b7236a5056554056d14e67
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 02:18:27 +0200

    adding influxdb

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,14 +47,7 @@ node grafana.ruby {
     proxy_set_header         =>   ['Host             $host', 'X-Real-IP        $remote_addr', 'X-Forwarded-For  $proxy_add_x_forwarded_for'],
   }
   -> class {'influxdb::server':}
-  http_conn_validator { 'grafana-conn-validator' :
-    host     => 'localhost',
-    port     => '3000',
-    use_ssl  => false,
-    test_url => '/public/img/grafana_icon.svg',
-    require  => Class['grafana'],
-  } ->
-  grafana_datasource { 'influxdb':
+  -> grafana_datasource { 'influxdb':
     grafana_url       => 'http://localhost:8080',
     grafana_user      => 'admin',
     grafana_password  => 'gfhfcjkmrf',
