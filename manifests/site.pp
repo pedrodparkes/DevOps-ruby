c465d24c548947daa38757184cf17b73d496d21b
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-13 02:14:46 +0200

    adding influxdb

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,27 +36,35 @@ node grafana.ruby {
       users    => {
         allow_sign_up => false,
       },
-      grafana_datasource { 'influxdb':
-        grafana_url       => 'http://localhost:3000',
-        grafana_user      => 'admin',
-        grafana_password  => '5ecretPassw0rd',
-        type              => 'influxdb',
-        url               => 'http://localhost:8086',
-        user              => 'admin',
-        password          => '1nFlux5ecret',
-        database          => 'graphite',
-        access_mode       => 'proxy',
-        is_default        => true,
-        json_data         => template('path/to/additional/config.json'),
-      }
     },
   }
-  class { 'nginx': }
+  -> class { 'nginx': }
   nginx::resource::upstream { 'grafana':
     members => ['localhost:8080'],
   }
   nginx::resource::server { 'grafana.glash.io':
     proxy => 'http://grafana',
     proxy_set_header         =>   ['Host             $host', 'X-Real-IP        $remote_addr', 'X-Forwarded-For  $proxy_add_x_forwarded_for'],
+  }
+  -> class {'influxdb::server':}
+  http_conn_validator { 'grafana-conn-validator' :
+    host     => 'localhost',
+    port     => '3000',
+    use_ssl  => false,
+    test_url => '/public/img/grafana_icon.svg',
+    require  => Class['grafana'],
+  } ->
+  grafana_datasource { 'influxdb':
+    grafana_url       => 'http://localhost:8080',
+    grafana_user      => 'admin',
+    grafana_password  => 'gfhfcjkmrf',
+    type              => 'influxdb',
+    url               => 'http://localhost:8086',
+    user              => 'admin',
+    password          => 'gfhfcjkmrf',
+    database          => 'graphite',
+    access_mode       => 'proxy',
+    is_default        => true,
+    #json_data         => template('path/to/additional/config.json'),
   }
 }
