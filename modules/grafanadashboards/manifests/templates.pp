7899e0ed2f009a3e5fbc3949131a12bad59ea349
Vitalii Samotaiev <vitalii.samotaiev@mcdean.com>
2017-12-14 19:18:15 +0200

    graf

--- modules/grafanadashboards/manifests/templates.pp
+++ modules/grafanadashboards/manifests/templates.pp
@@ -3,13 +3,6 @@
 class grafanadashboards::templates inherits grafanadashboards {
 
   file { '/etc/grafana/templates':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-
-  file { '/etc/grafana/templates':
     ensure  => directory,
     recurse => true,
     source  => "puppet:///modules/${module_name}/files",
